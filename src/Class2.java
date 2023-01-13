251cbd1b9d85dde7aacd3fbc6970bbfa7b66a315
aakash.tawde8 <aakash.tawde00@gmail.com>
2023-01-14 00:44:42 +0530

    newline added

--- src/Class2.java
+++ src/Class2.java
@@ -3,5 +3,6 @@ public class Class2 {
         System.out.println("Noob2");
         System.out.println("Noob2.1");
         System.out.println("Aakash");
+        System.out.println("New change 3");
     }
 }
