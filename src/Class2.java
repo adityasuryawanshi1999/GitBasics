52a750ccb2df79ca6d2b311e25eb15bd86b0358a
aakash.tawde8 <aakash.tawde00@gmail.com>
2023-01-14 00:38:58 +0530

    Aakash noob added

--- src/Class2.java
+++ src/Class2.java
@@ -2,5 +2,6 @@ public class Class2 {
     public static void main(String[] args) {
         System.out.println("Noob2");
         System.out.println("Noob2.1");
+        System.out.println("Aakash");
     }
 }
